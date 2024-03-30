98c503ac04ce1aa89ff2b955e585e748c06d99c9
Enzoronovich <137046327+Enzoronovich@users.noreply.github.com>
2024-03-30 14:48:50 +0800

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,36 @@
+<?php
+
+$fname = $_post["First name"];
+$lname = $_post["Last name"];
+$Email = $_post["Email"];
+$contact = $_post["Contact number"];
+$subject = $_post["Subject"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "lyle.damien17@gmail.com.com";
+$mail->Password = "bsxa pjbx eqpe lzmb";
+
+$mail->setFrom($Email, $fname, $contact, $subject, $message );
+$mail->addAddress("dave@example.com", "Dave");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
